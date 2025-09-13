8d349e44161d9dd9e78fd6768471832de5df96b3
Essam Ewaisha <essamemad16@gmail.com>
2025-09-13 09:00:24 +0100

    [flux] Install image controllers
    This diff installs flux image controllers to allow flux to automatically update images from container registries.

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -1,15 +1,15 @@
 provider "flux" {
-    kubernetes = {
-        config_path = local_file.kubeconfig.filename
-    }
+  kubernetes = {
+    config_path = local_file.kubeconfig.filename
+  }
 
-    git = {
-        url = "https://github.com/alrimaal/IaasOps"
-        http = {
-            username = "git" # Using PAT so any string works
-            password = data.bitwarden_secret.flux_github_token.value
-        }
+  git = {
+    url = "https://github.com/alrimaal/IaasOps"
+    http = {
+      username = "git" # Using PAT so any string works
+      password = data.bitwarden_secret.flux_github_token.value
     }
+  }
 }
 
 provider "github" {
@@ -19,12 +19,13 @@ provider "github" {
 
 
 resource "flux_bootstrap_git" "this" {
-  path               = "fluxcd/cluster/production"
+  path = "fluxcd/cluster/production"
   timeouts = {
     create = "1m"
-    read = "1m"
+    read   = "1m"
     delete = "1m"
     update = "1m"
   }
-  depends_on = [ module.talos, local_file.kubeconfig, local_file.talosconfig ]
+  components_extra = ["image-reflector-controller", "image-automation-controller"]
+  depends_on       = [module.talos, local_file.kubeconfig, local_file.talosconfig]
 }
